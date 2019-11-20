2f1b9b37de75c640a53edbd1d06a423aa578b7e7
Isaac <aluno@estacio.acad>
2019-11-20 16:41:44 -0300

    Segundo commit

--- Revisao-PHP-Exercicios/index.php
+++ Revisao-PHP-Exercicios/index.php
@@ -2,13 +2,23 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Exercício 1</title>
+    <title>Exercícios PHP</title>
   </head>
   <body>
-  <h1><a href="Exercicio_01">Exercício 01</a></h1>
+	<h1>Exercícios para pensar em PHP</h1>
+	<hr>
+	<ul>
+  	<li><p><a href="#exercicio_01">Exercício 01</a></p></li>
+		<li><p><a href="#exercicio_02">Exercício 02</a></p></li>
+		<li><p><a href="#exercicio_03">Exercício 03</a></p></li>
+	</ul>
+
+	<hr>
+
     <div id="exercicio_01">
-      <hr>
-      <h2>Exercício 01</h2>
+      
+      <h3>Exercício 01</h3>
+			
         <p> 
           Construir um algoritmo que leia 2 números e
           efetue a adição. Caso o valor somado seja
@@ -24,9 +34,44 @@
         <input type="submit" name="btn" value="Calcular">
       
       </form>
-
-    
+ 
     </div>
-    
+		<hr>
+
+	<div id="exercicio_02">
+		<h3>Exercício 02</h3>
+			<p>
+				Entrar com um número e informar se ele é divisível por 10, por 5, por 2 ou se não é divisível por nenhum destes.
+			</p>
+
+		<form action="script.php" method="POST">
+			Digite um número: <input type="text" placeholder="Digite o número..." name="num_3">
+			<input type="submit" name="btn_2" value="Entra"><br>
+		</form>
+	</div>
+	<hr>
+
+	<div id="exercicio_03">
+		<h3>Exercício 03</h3>
+			<p>
+				Entrar com nome, sexo e idade de uma pessoa. Se a pessoa for do sexo feminino e tiver menos que 25 anos, imprimir nome e a a mensagem: ACEITA. Caso contrário, imprimir nome e a mensagem: NÃO ACEITA.
+			</p>
+			<form action="script.php" method="POST">
+				Nome: <input type="Text" name="nome"><br>
+				Idade: <input ype="number" name="idade"><br>
+				<input type="radio" name="sexo"
+				<?php if (isset($gender) && $gender=="female") echo "checked";?> value="Feminino">Feminino
+				<input type="radio" name="sexo"
+				<?php if (isset($gender) && $gender=="masculino") echo "checked";?>
+				value="masculino">Masculino
+				<input type="radio" name="sexo"
+				<?php if (isset($gender) && $gender=="outro") echo "checked";?>
+				value="outro">Outro
+			</form>
+
+	</div>
+
+
+		<footer><br>Desenvolvido por Isaac Belo</footer>
   </body>
 </html>
--- Revisao-PHP-Exercicios/script.php
+++ Revisao-PHP-Exercicios/script.php
@@ -1,17 +1,39 @@
 <?php
-  $num1 = $_POST["num_1"];
-  $num2 = $_POST["num_2"];
+	// Script do Exercício 01
+	if(isset($_POST["btn"])) {
+		$num1 = $_POST["num_1"];
+		$num2 = $_POST["num_2"];
 
-  $soma = $num1 + $num2;
+		$soma = $num1 + $num2;
 
-  echo "O Valor da soma de $num1 + $num2 = $soma<br>";
+		echo "O Valor da soma de $num1 + $num2 = $soma<br>";
 
-  if($soma > 20)
-  {
-    $soma = $soma + 8;
-    echo "Maior que 20: <br>Total = $soma";
-  } else {
-    $soma = $soma -5;
-    echo "Menor/igual a 20: <br>Total = $soma";
-  }
+		if($soma > 20)
+		{
+			$soma = $soma + 8;
+			echo "Maior que 20: <br>Total = $soma";
+		} else {
+			$soma = $soma -5;
+			echo "Menor/igual a 20: <br>Total = $soma";
+		}
+	}
+	// Script do Exercício 02
+	if(isset($_POST["btn_2"])) {
+		$num3 = $_POST["num_3"];
+
+		if($num3 % 10 == 0) {
+			echo "O número $num3 é divisível por 10, 5 e 2! <br>";
+		} else if($num3 % 5 == 0) {
+			echo "O número $num3 é divisível por 5!<br>";
+		} else if($num3 % 2 == 0) {
+			echo "O número $num3 é divisível por 2!<br>";
+		} else {
+			echo "O número $num3 não é divisível por 2, 5 ou 10";
+		}
+	}
+	//Script do Exercício 03
+
+
+
+	echo "<br><a href='index.php'> <= Voltar</a>"
 ?>
